7192c39337871c0e4444ac25bcd25b12f0b3a0c3
seantrytech <sean.mysell@trytech.com.au>
2018-03-15 22:44:57 +1100

    First commit

---
+++ dpindex.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+    <script language="JavaScript">
+        var loc = window.location.href + '';
+        if (loc.indexOf('http://') == 0) {
+            window.location.href = loc.replace('http://', 'https://');
+        }
+    </script>
+    <title>
+        <?php echo $_SERVER['HTTP_HOST'];?> is a registered client of Digital Pacific
+    </title>
+
+    <meta name="Generator" content="" />
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <meta name="verify-v1" content="Y3JbpyB/hJfxHOCBf9cL6JBegLhF/aHXYpkqmoT3ePw=" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <link rel="shortcut icon" href="//www.digitalpacific.com.au/templateimages/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="all" href="//www.digitalpacific.com.au/cpanel-placeholder/css/style.css" />
+
+    <body>
+        <div id="container">
+            <img src="//www.digitalpacific.com.au/cpanel-placeholder/images/digitalpacific.jpg" alt="Registered Client of Digital Pacific" />
+            <p>
+                The domain name
+                <?php echo $_SERVER['HTTP_HOST'];?> is a registered client of <br /> Digital Pacific PTY LTD <br />
+                :: <a href="//www.digitalpacific.com.au/domain-names/register-domain-name/">domain names</a>
+                :: <a href="//www.digitalpacific.com.au/hosting/overview/">web hosting</a>
+                :: <a href="//www.digitalpacific.com.au/hosting/dedicated-hosting/virtual-private-servers/">virtual private servers</a>
+                :: <a href="//www.digitalpacific.com.au/hosting/dedicated-hosting/dedicated-servers/">dedicated servers</a>
+            </p>
+
+            <p>
+                If you are the owner of this domain you can login <a href="//<?php echo $_SERVER['HTTP_HOST'];?>/cpanel/">here</a> to start using your hosting account.
+            </p>
+        </div>
+    </body>
+
+</html>
